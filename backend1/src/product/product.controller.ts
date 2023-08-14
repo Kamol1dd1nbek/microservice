19f1dedc45f212b2a106f721ed404594fe6876da
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-14 10:23:44 +0500

    microservice connected to RMQ Cloud Server

--- backend1/src/product/product.controller.ts
+++ backend1/src/product/product.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ClientProxy } from "@nestjs/microservices"
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(
+    private readonly productService: ProductService,
+    @Inject("PRODUCT_SERVICE") private readonly client: ClientProxy
+    ) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  async create(@Body() createProductDto: CreateProductDto) {
+     const product = await this.productService.create(createProductDto);
+     console.log(product);
+
+     this.client.emit("product_created", product);
+     return product;
   }
 
   @Get()
   findAll() {
+    this.client.emit('hello', "Hello from another server");
     return this.productService.findAll();
   }
 
--- backend1/src/product/product.module.ts
+++ backend1/src/product/product.module.ts
@@ -3,10 +3,24 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { Transport, ClientsModule } from '@nestjs/microservices';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ Product ])
+    TypeOrmModule.forFeature([ Product ]),
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqps://gapwmdal:kIspfXDETGc0WLvc6Yx_9Ogfu0hgyVan@toad.rmq.cloudamqp.com/gapwmdal'],
+          queue: 'main_products_queue',
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+    ]),
   ],
   controllers: [ProductController],
   providers: [ProductService],
